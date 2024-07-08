392da9a258f31b47b32848090443813e0edbc3b2
SANTIAGO ALEJANDRO PALMA <47490026@ort>
2024-07-08 11:02:53 -0300

    endpoints prestadores creados

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config = require("./config/dbConfig");
+const config = require("./src/config/dbConfig");
 const express = require("express");
 const cors = require("cors");
 const sql = require("mssql");
@@ -15,29 +15,16 @@ const getConnection = async () => {
     return pool;
   } catch (error) {
     console.error("Error al conectar con la base de datos:", error.message, error.code);
-
   }
 }; 
 
-// Ruta para obtener los casos activos desde la base de datos
+// Ruta CASOS, para mover al controller
 app.get("/casos", async (req, res) => {
   try {
     const pool = await getConnection();
     if (pool) {
-      const result = await sql.query(`
-            SELECT 
-                C.IdCaso,
-                C.FechaOcurrencia,
-                C.FechaSolicitud,
-                C.Diagnostico,
-                P.Nombre AS NombrePaciente,
-                PR.Nombre AS NombrePrestador,
-                PS.Nombre AS NombrePrestacion
-            FROM 
-                Caso C
-                INNER JOIN Paciente P ON C.IdPaciente = P.IdPaciente
-                INNER JOIN Prestador PR ON C.IdPrestador = PR.IdPrestador
-                INNER JOIN Prestacion PS ON C.IdPrestacion = PS.IdPrestacion`);
+      const request = pool.request();
+      const result = await request.execute("getCasos")
       res.json(result.recordset);
     } else {
       res.status(500).json({ message: "No se pudo establecer conexión con la base de datos" });
@@ -53,22 +40,9 @@ app.get("/casos/:id", async (req,res)=>{
     const pool =await getConnection();
     if(pool){
       if (pool) {
-        const result = await sql.query(`
-              SELECT 
-                  C.IdCaso,
-                  C.FechaOcurrencia,
-                  C.FechaSolicitud,
-                  C.Diagnostico,
-                  P.Nombre AS NombrePaciente,
-                  PR.Nombre AS NombrePrestador,
-                  PS.Nombre AS NombrePrestacionX
-              FROM 
-                  Caso C
-                  INNER JOIN Paciente P ON C.IdPaciente = P.IdPaciente
-                  INNER JOIN Prestador PR ON C.IdPrestador = PR.IdPrestador
-                  INNER JOIN Prestacion PS ON C.IdPrestacion = PS.IdPrestacion
-              WHERE
-                  C.IdCaso = ${id}`);
+        const request = pool.request();
+        request.input("IdCaso",sql.Int,id)
+        const result = await request.execute("getCasoById")
               
       res.json(result.recordset);
     }
@@ -81,11 +55,43 @@ app.get("/casos/:id/devolucion", async (req,res)=>{
   if(pool){
     if (pool) {
       console.log("hola")
-      const result = await sql.query(`SELECT * FROM InformeDia`);
+      const result = await sql.query(`SELECT * FROM InformeDia WHERE IdCaso = ${id}`);
     res.json(result.recordset);
     console.log(result)
   }
 }
+})
+
+// Ruta PRESTADORES, para mover al controller
+
+app.get("/prestadores", async (req, res) => {
+  try {
+    const pool = await getConnection();
+    if (pool) {
+      const request = pool.request();
+      const result = await request.execute("getPrestadores")
+      res.json(result.recordset);
+    } else {
+      res.status(500).json({ message: "No se pudo establecer conexión con la base de datos" });
+    }
+  } catch (error) {
+    console.error("Error al obtener los casos activos:", error);
+    res.status(500).json({ message: "Error interno del servidor" });
+  }
+});
+
+app.get("/prestadores/:id", async (req,res)=>{
+    const id = parseInt(req.params.id)
+    const pool =await getConnection();
+    if(pool){
+      if (pool) {
+        const request = pool.request();
+        request.input("IdCaso",sql.Int,id)
+        const result = await request.execute("getPrestadorById")
+              
+      res.json(result.recordset);
+    }
+  }
 })
 
 // Puerto en el que escucha el servidor
---
+++ src/config/dbConfig.js
@@ -0,0 +1,11 @@
+const config = {
+    user : 'alumno',
+    password: 'alumno',
+    server: 'A-NHFY-INFO-17',
+    database: 'HomeCareProBD',
+    options: {
+        trustServerCertificate : true,
+        trustedConnection : true
+    }
+}
+module.exports = config;
