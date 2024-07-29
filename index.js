8fb5f9a6ad3bfc6e3af8de61a00f15bcbafc4394
RAMIRO SUED <47958668@ort>
2024-07-29 12:09:31 -0300

    avances

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config = require("./src/config/dbConfig");
+const config = require("./config/dbConfig");
 const express = require("express");
 const cors = require("cors");
 const sql = require("mssql");
@@ -15,16 +15,29 @@ const getConnection = async () => {
     return pool;
   } catch (error) {
     console.error("Error al conectar con la base de datos:", error.message, error.code);
+
   }
 }; 
 
-// Ruta CASOS, para mover al controller
+// Ruta para obtener los casos activos desde la base de datos
 app.get("/casos", async (req, res) => {
   try {
     const pool = await getConnection();
     if (pool) {
-      const request = pool.request();
-      const result = await request.execute("getCasos")
+      const result = await sql.query(`
+            SELECT 
+                C.IdCaso,
+                C.FechaOcurrencia,
+                C.FechaSolicitud,
+                C.Diagnostico,
+                P.Nombre AS NombrePaciente,
+                PR.Nombre AS NombrePrestador,
+                PS.Nombre AS NombrePrestacion
+            FROM 
+                Caso C
+                INNER JOIN Paciente P ON C.IdPaciente = P.IdPaciente
+                INNER JOIN Prestador PR ON C.IdPrestador = PR.IdPrestador
+                INNER JOIN Prestacion PS ON C.IdPrestacion = PS.IdPrestacion`);
       res.json(result.recordset);
     } else {
       res.status(500).json({ message: "No se pudo establecer conexión con la base de datos" });
@@ -40,9 +53,22 @@ app.get("/casos/:id", async (req,res)=>{
     const pool =await getConnection();
     if(pool){
       if (pool) {
-        const request = pool.request();
-        request.input("IdCaso",sql.Int,id)
-        const result = await request.execute("getCasoById")
+        const result = await sql.query(`
+              SELECT 
+                  C.IdCaso,
+                  C.FechaOcurrencia,
+                  C.FechaSolicitud,
+                  C.Diagnostico,
+                  P.Nombre AS NombrePaciente,
+                  PR.Nombre AS NombrePrestador,
+                  PS.Nombre AS NombrePrestacionX
+              FROM 
+                  Caso C
+                  INNER JOIN Paciente P ON C.IdPaciente = P.IdPaciente
+                  INNER JOIN Prestador PR ON C.IdPrestador = PR.IdPrestador
+                  INNER JOIN Prestacion PS ON C.IdPrestacion = PS.IdPrestacion
+              WHERE
+                  C.IdCaso = ${id}`);
               
       res.json(result.recordset);
     }
@@ -55,43 +81,31 @@ app.get("/casos/:id/devolucion", async (req,res)=>{
   if(pool){
     if (pool) {
       console.log("hola")
-      const result = await sql.query(`SELECT * FROM InformeDia WHERE IdCaso = ${id}`);
+      const result = await sql.query(`SELECT * FROM InformeDia`);
     res.json(result.recordset);
     console.log(result)
   }
 }
 })
 
-// Ruta PRESTADORES, para mover al controller
-
-app.get("/prestadores", async (req, res) => {
-  try {
-    const pool = await getConnection();
+app.get("/medico/:id", async (req,res)=>{
+  const id = parseInt(req.params.id)
+  const pool =await getConnection();
+  if(pool){
     if (pool) {
-      const request = pool.request();
-      const result = await request.execute("getPrestadores")
-      res.json(result.recordset);
-    } else {
-      res.status(500).json({ message: "No se pudo establecer conexión con la base de datos" });
-    }
-  } catch (error) {
-    console.error("Error al obtener los casos activos:", error);
-    res.status(500).json({ message: "Error interno del servidor" });
-  }
-});
+      console.log("hola")
+      const result = await sql.query( `
+      SELECT c.FechaOcurrencia, c.FechaSolicitud, c.Diagnostico, c.CantDias, c.CantHorasDias, c.EnCurso, c.Idcaso, pa.Dni, pa.Nombre, pa.Apellido, pa.Direccion, pa.Localidad, pa.Telefono, pa.FechaNacimiento, pr.Nombre as NombrePrestador
+      FROM Caso AS c INNER JOIN Paciente as pa ON pa.IdPaciente = c.IdPaciente
+      INNER JOIN Prestador as pr ON pr.IdPrestador = c.IdPrestador
+      
+      WHERE c.IdPrestador = ${id}
+    `)
+    console.log(result.recordset)
+    res.json(result.recordset);
 
-app.get("/prestadores/:id", async (req,res)=>{
-    const id = parseInt(req.params.id)
-    const pool =await getConnection();
-    if(pool){
-      if (pool) {
-        const request = pool.request();
-        request.input("IdCaso",sql.Int,id)
-        const result = await request.execute("getPrestadorById")
-              
-      res.json(result.recordset);
-    }
   }
+}
 })
 
 // Puerto en el que escucha el servidor
