a9a5ed6f095d12b34d73ebaa42be45a03c97a8f1
RAMIRO SUED <47958668@ort>
2024-09-13 10:37:01 -0300

    creacion un controller

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import CasoController from "./src/controllers/caso-controller.js";;
 import MedicoController from "./src/controllers/medico-controller.js";;
+import PrestadorController from "./src/controllers/prestador-controller.js";;
 
 import config from "./src/config/dbConfig.js";
 import { MailerSend } from 'mailersend';
@@ -16,6 +17,7 @@ app.use(express.json());
 // Configuración de la conexión a la base de datos MSSQL
 app.use("/casos", CasoController)
 app.use("/medicos", MedicoController)
+app.use("/prestadores", PrestadorController)
 
 
 
@@ -212,47 +214,6 @@ app.post('/medicoooo/codigo', async (req, res) => {
 
 
 
-app.post("/prestadores", async (req, res) => {
-  const { dni, nombre, apellido, direccion, localidad, telefono, email, genero, contraseña } = req.body;
-
-  if (!nombre || !localidad || !dni || !apellido || !telefono || !email || !direccion || !genero || !contraseña) {
-    return res.status(400).json({ error: "Faltan datos" });
-  }
-
-  try {
-    const pool = await getConnection();
-
-    if (!pool) {
-      return res.status(500).json({ error: "No se pudo conectar a la base de datos" });
-    }
-
-    // Inserción en la tabla Paciente
-    const query1 = `
-      INSERT INTO Prestador (IdEspecialidad, Dni, Nombre, Apellido, Direccion, Localidad, Telefono, Email, Genero, Contraseña)
-      VALUES (@idEspecialidad, @dni, @nombre, @apellido, @direccion, @localidad, @telefono, @email, @genero, @contraseña);
-    `;
-    await pool.request()
-      .input('idEspecialidad', sql.Int, 1) // Cambiado a dni
-      .input('dni', sql.Int, dni) // Cambiado a dni
-      .input('nombre', sql.NVarChar, nombre)
-      .input('apellido', sql.NVarChar, apellido)
-      .input('direccion', sql.NVarChar, direccion)
-      .input('localidad', sql.NVarChar, localidad)
-      .input('telefono', sql.Int, parseInt(telefono))
-      .input('email', sql.NVarChar, email)
-      .input('genero', sql.NVarChar, genero)
-      .input('contraseña', sql.NVarChar, contraseña)
-      .query(query1);
-
-
-  
-    res.status(201).json({ message: "Prestador creado exitosamente" });
-
-  } catch (error) {
-    console.error("Error en la inserción:", error);
-    res.status(500).json({ error: "Error en la inserción", details: error.message });
-  }
-});
 
 
 // Puerto en el que escucha el servidor
---
+++ src/controllers/prestador-controller.js
@@ -0,0 +1,61 @@
+import { Router } from "express";
+import config from "../config/dbConfig.js";
+import sql from "mssql";
+
+let router = Router();
+const getConnection = async () => {
+  try {
+    const pool = await sql.connect(config); //http://localhost:5000
+    return pool;
+  } catch (error) {
+    console.error("Error al conectar con la base de datos:", error.message, error.code);
+
+  }
+}; 
+
+
+app.post('', async (req, res) => {
+    const { dni, nombre, apellido, direccion, localidad, telefono, email, genero, contraseña } = req.body;
+  
+    if (!nombre || !localidad || !dni || !apellido || !telefono || !email || !direccion || !genero || !contraseña) {
+      return res.status(400).json({ error: "Faltan datos" });
+    }
+  
+    try {
+      const pool = await getConnection();
+  
+      if (!pool) {
+        return res.status(500).json({ error: "No se pudo conectar a la base de datos" });
+      }
+  
+      // Inserción en la tabla Paciente
+      const query1 = `
+        INSERT INTO Prestador (IdEspecialidad, Dni, Nombre, Apellido, Direccion, Localidad, Telefono, Email, Genero, Contraseña)
+        VALUES (@idEspecialidad, @dni, @nombre, @apellido, @direccion, @localidad, @telefono, @email, @genero, @contraseña);
+      `;
+      await pool.request()
+        .input('idEspecialidad', sql.Int, 1) // Cambiado a dni
+        .input('dni', sql.Int, dni) // Cambiado a dni
+        .input('nombre', sql.NVarChar, nombre)
+        .input('apellido', sql.NVarChar, apellido)
+        .input('direccion', sql.NVarChar, direccion)
+        .input('localidad', sql.NVarChar, localidad)
+        .input('telefono', sql.Int, parseInt(telefono))
+        .input('email', sql.NVarChar, email)
+        .input('genero', sql.NVarChar, genero)
+        .input('contraseña', sql.NVarChar, contraseña)
+        .query(query1);
+  
+  
+    
+      res.status(201).json({ message: "Prestador creado exitosamente" });
+  
+    } catch (error) {
+      console.error("Error en la inserción:", error);
+      res.status(500).json({ error: "Error en la inserción", details: error.message });
+    }
+  });
+
+
+
+export default router;
